ff284c92cb1edbe0543da217d04d92f84414e212
Suhas Thalanki <suhastvs@gmail.com>
2021-04-25 22:12:25 +0530

    Added functionality to add/delete keywords

--- bot.py
+++ bot.py
@@ -1,13 +1,23 @@
 import discord
 from discord.ext import commands
 from web_scrape import motion_scrape
+import csv
 
 # Create a bot instance and sets a command prefix
 client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
 
+# Initialising Keywords
+keys = list()
+
 @client.event
 async def on_ready():
+    # Creating csv file
+    global keys
+    with open("keywords.csv", "r") as inputfile:
+        for row in csv.reader(inputfile):
+            keys.append(row[0])
+
     print('Bot is ready')
 
 @client.command(aliases = ['test'])
@@ -18,7 +28,7 @@ async def ping(ctx):
 async def help(ctx):
     embed=discord.Embed(title = 'Debate Motions Bot', description = 'A bot that gives the user Parliamentary Debate Motions')
     motions = 'List of motions, keywords and number are optional parameters. Default is 5.'
-    keywords = 'List of available keywords and the abbreviation to be used'
+    keywords = 'List of available keywords'
     random = 'List of motions, randomly chosen'
     embed.add_field(name = 'motions `keywords` `number`', value = motions, inline = True)
     embed.add_field(name = 'Keywords', value = keywords, inline = True)
@@ -27,17 +37,9 @@ async def help(ctx):
 
 @client.command()
 async def keywords(ctx):
-    theme = ['International Relations',
-            'Economics',
-            'Feminism',
-            'Narratives',
-            'Policy',
-            "United States",
-            "India",
-            "Criminal Justice"]
-    
-    s = '**Available Themes:**\n'
-    for i in theme:
+    global keys
+    s = '**Available Keywords:**\n'
+    for i in keys:
         s += i + '\n'
 
     await ctx.send(s)
@@ -55,9 +57,29 @@ async def motions(ctx, *, words):
     else:
         await ctx.send(motion_scrape(words))
 
-    
+@client.command(aliases=['add'])
+async def key_append(ctx, keyword):
+    global keys
+    # To add a keyword to the list
+    keys.append(keyword)
+    # Updating csv file
+    keyword += '\n'
+    with open("keywords.csv", 'a') as inputfile:
+        inputfile.write(keyword)
 
-    
+@client.command(aliases=['delete'])
+async def key_delete(ctx, keyword):
+    global keys
+    # To delete a keyword from the list
+    try:
+        keys.remove(keyword)
+        # Overwrite csv file
+        with open('keywords.csv', 'w') as inputfile:
+            writer = csv.writer(inputfile)
+            for i in keys:
+                writer.writerow(i)  
+    except:
+        await ctx.send("Keyword not in given list. Use `.keywords` to get the list of available keywords")
 
 
 # Runs the client (bot)
