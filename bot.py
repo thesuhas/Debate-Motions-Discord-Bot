e46e284c330d47c362a74c971336e94c4f32c50c
Suhas Thalanki <suhastvs@gmail.com>
2021-04-25 11:57:10 +0530

    Basic help function

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 # Create a bot instance and sets a command prefix
 client = commands.Bot(command_prefix = '.')
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -11,6 +12,18 @@ async def on_ready():
 @client.command(aliases = ['test'])
 async def ping(ctx):
     await ctx.send(f"{round(client.latency * 1000)} ms")
+
+@client.command()
+async def help(ctx):
+    embed=discord.Embed(title = 'Debate Motions Bot', description = 'A bot that gives the user Parliamentary Debate Motions')
+    motions = 'List of motions, theme and number are optional parameters. Default is 5.'
+    theme = 'List of available themes and the abbreviation to be used'
+    random = 'List of motions, randomly chosen'
+    embed.add_field(name = 'motions `theme` `number`', value = motions, inline = True)
+    embed.add_field(name = 'themes', value = theme, inline = True)
+    embed.add_field(name = 'random', value = random)
+    await ctx.send(embed = embed)
+
 
 
 # Runs the client (bot)
