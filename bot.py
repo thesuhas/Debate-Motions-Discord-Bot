a1a00b41fa47cfa13ff02c626196b267a901ff5f
Suhas Thalanki <suhastvs@gmail.com>
2021-04-25 11:36:11 +0530

    added ping command

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready')
 
+@client.command(aliases = ['test'])
+async def ping(ctx):
+    await ctx.send(f"{round(client.latency * 1000)} ms")
+
 
 # Runs the client (bot)
 client.run('ODM1NzUyNjU3MTM4MzUyMTM4.YIUBUA.w3xIDb4hOwOSpBbKOiJzs1-CMAo')
